1c0af0da29f8c95e9fd4a8fa081c74dd49a73d46
student <pn.gerebennikova@gmail.com>
2023-11-20 15:58:28 +0900

    uhu

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+
+import random
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(random.randrange(0, 300), random.randrange(0, 200), random.randrange(0, 100), random.randrange(0, 100))
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
